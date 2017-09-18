a7f2623132abfc7ab229d96a12224aff34929a52
Tanja Leonova <tanja.v.leonova@gmail.com>
2017-09-19 01:57:02 +0200

    two important files in Business

---
+++ Portfolio/Portfolio/Business/DependencyResolverInitialization.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using System.Web.Mvc;
+using StructureMap;
+
+namespace Portfolio.Business
+{
+    [InitializableModule]
+    public class DependencyResolverInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Container.Configure(ConfigureContainer);
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(context.Container));
+        }
+
+        private static void ConfigureContainer(ConfigurationExpression container)
+        {
+            //Swap out the default ContentRenderer for our custom
+            //container.For<IContentRenderer>().Use<ErrorHandlingContentRenderer>();
+
+            //Implementations for custom interfaces can be registered here.
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        public void Preload(string[] parameters)
+        {
+        }
+    }
+}
